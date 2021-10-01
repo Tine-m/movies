816de3c2a68bcc37beef7f1dfdf117b92418cda9
Tine Marbjerg <tinemarbjerg@gmail.com>
2021-10-01 14:06:27 +0200

    movies demo

---
+++ movieapp/src/main/java/movieapp/services/MovieService.java
@@ -0,0 +1,19 @@
+package movieapp.services;
+
+import movieapp.dataFacade.MovieRepository;
+import movieapp.models.Movie;
+
+import java.util.List;
+
+public class MovieService {
+
+    private MovieRepository movieRepository = new MovieRepository();
+
+    public List<Movie> findAllMovies() {
+        return movieRepository.findAllMovies();
+    }
+
+    public Movie findMovieByTitle(String title) {
+        return movieRepository.findMovieByTitle(title);
+    }
+}
